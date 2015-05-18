348f4aa37a022e88b93f41dd2ca11fb2fb726c14
Nick Matvyeyev <mnasoft@gmail.com>
2015-05-18 13:00:52 +0300

    Изменен файл .gitignore

--- html/lib/config_class.php
+++
@@ -1,9 +0,0 @@
-<?php
-abstract class Config {
-  const DB_HOST         = "";
-  const DB_USER         = "";
-  const DB_PASSWORD     = "";
-  const DB_NAME         = "coating";
-  const DB_CHARSET      = "utf8";
-}
-?>
