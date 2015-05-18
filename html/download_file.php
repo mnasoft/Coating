761cb621bf88d4d37704b85dee7dba14825d19db
Nick Matvyeyev <mnasoft@gmail.com>
2015-05-18 12:48:36 +0300

    Выполнена первоначальная настройка выгрузки файла на сервер

--- html/download_file.php
+++ html/download_file.php
@@ -1,17 +1,18 @@
 <?php
 // download_file.php
-require_once 'start.php';
-$cnt=new coating(Config::DB_HOST, Config::DB_USER, Config::DB_PASSWORD, Config::DB_NAME , Config::DB_CHARSET);
+// require_once 'start.php';
+// $cnt=new coating(Config::DB_HOST, Config::DB_USER, Config::DB_PASSWORD, Config::DB_NAME , Config::DB_CHARSET);
 
-$cnt->page_start("PROTECTIVE COATINGS DATA. Download_file.");
+// $cnt->page_start("PROTECTIVE COATINGS DATA. Download_file.");
 ?>
+
 <form enctype="multipart/form-data" action="move_file.php" method="POST">   <!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
-  <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />             <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
+  <input type="hidden" name="MAX_FILE_SIZE" value="83886080" />             <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
   Отправить этот файл: <input name="userfile" type="file" />              <!-- Название элемента input определяет имя в массиве $_FILES -->
   <input type="submit" value="Send File" />
-  </form>
+</form>
 
-  <?php
-  $cnt->page_end();
+<?php
+// $cnt->page_end();
 ?>
 
--- html/move_file.php
+++ html/move_file.php
@@ -1,5 +1,6 @@
 <?php
 // move_file.php
+/*
 require_once 'start.php';
 
 $cnt=new coating(Config::DB_HOST, Config::DB_USER, Config::DB_PASSWORD, Config::DB_NAME , Config::DB_CHARSET);
@@ -7,7 +8,7 @@ $cnt=new coating(Config::DB_HOST, Config::DB_USER, Config::DB_PASSWORD, Config::
 $cnt->page_start("move_file");
 
 $uploaddir = '/home/namatv/uploads/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+ $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
 echo '<pre>';
 echo "uploaddir = $uploaddir\n";
@@ -30,4 +31,58 @@ print_r($_FILES);
 print "</pre>";
 
 $cnt->page_end();
+*/
+?>
+
+<?php
+
+// В PHP 4.1.0 и более ранних версиях следует использовать $HTTP_POST_FILES
+// вместо $_FILES.
+
+
+function rename_file_name($filename)
+{
+  $date = microtime();
+  $date_array = explode(" ", $date);
+  $date_y = date("Y-m-d-H-i",$date_array[1]);
+  $date_s = date("s", $date_array[1]) + $date_array[0] ;
+  $new_file_name =
+    pathinfo($filename, PATHINFO_DIRNAME) .
+    "/" .
+    $date_y .
+    "-" .
+    $date_s .
+    "-" .
+    md5_file($filename) .
+    "." .
+    pathinfo($filename, PATHINFO_EXTENSION);
+
+  echo $filename . "\n";
+  echo $new_file_name . "\n";
+  
+  rename($filename, $new_file_name );
+  return true;
+}
+
+$uploaddir = '/home/namatv/public_html/upload/'; // /var/www/uploads/
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+
+echo 'TScoooool :'. md5_file($_FILES['userfile']['name']) . "\n"; 
+
+echo '<pre>';
+
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile))
+  {
+    echo "Файл корректен и был успешно загружен.\n";
+    rename_file_name($uploadfile);
+    echo "Файл корректен и был успешно загружен.\n";
+  }
+else
+  {
+    echo "Возможная атака с помощью файловой загрузки!\n";
+    echo 'Некоторая отладочная информация:';
+    print_r($_FILES);
+  }
+print "</pre>";
 ?>
