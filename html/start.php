4e284865235d0f2de1df0394cc909e44f3b8689e
Nick Matvyeyev <mnasoft@gmail.com>
2015-05-18 23:12:39 +0300

    Удалена отладочная информация

--- html/start.php
+++ html/start.php
@@ -2,7 +2,7 @@
 // start.php
 mb_internal_encoding("UTF-8");
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
+//ini_set("display_errors", 1);
 
 //   set_include_path(get_include_path().PATH_SEPARATOR."core".PATH_SEPARATOR."lib".PATH_SEPARATOR."objects".PATH_SEPARATOR."validator".PATH_SEPARATOR."controllers".PATH_SEPARATOR."modules");
 set_include_path(get_include_path().PATH_SEPARATOR."core".PATH_SEPARATOR."lib");
